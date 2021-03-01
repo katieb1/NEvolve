35feca70c3bd34deb42932dbd95bf992e22836c0
Katie Birchard <birchardkatie@gmail.com>
2021-03-01 12:59:19 -0800

    Adding profiler script to first draft CNN

--- CNN/CNN_draft1.py
+++ CNN/CNN_draft1.py
@@ -8,120 +8,89 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+# Import custom function
+import msms
+
 tic = time.perf_counter()
 
-### data loader will need to be modified!! ###
-# Currently using toy data set
-with np.load('./toy_dat/sliced_data.npz') as data:
-    param = data['param']
-    raw_sim = data['sim']
-    pos = data['pos']
-	
-# Normalize to mean of 0, sd of 1
+# Create train function to run data through nn    
+def main():
+    
+    # Start timing
+    tic = time.perf_counter()
+    
+    # Initialize dataset
+    ds = msms.Dataset("../pipeline/data",
+                      "../pipeline/metadata/lookup.npy",
+                      "../pipeline/metadata/ms_param.csv")
+    
+    # Initialize data loader
+    dataloader = torch.utils.data.DataLoader(ds,
+                                             batch_size=20,
+                                             shuffle=False, # NO SHUFFLE
+                                             num_workers=0)
+    
+    # Set network parameters
+    pool_size = 1
+    channels = [4, 6, 8]
+    kernels = [5, 4, 3]
+    nodes = [500, 100, 5]
 
-sim = (raw_sim - 0.5) / 0.5 # May need something more efficient for the full set?
-# print(raw_sim[0,1])
-# print(sim[0,1])
+    # Create model and wrap in DDP
+    net = msms.Net(ds.num_indivs, ds.num_sites, pool_size,
+                   channels, kernels, nodes).cuda()
+    
+    # Define criterion and optimizer functions
+    optimizer = torch.optim.SGD(net.parameters(), 1e-4)
+    total_step = len(dataloader)
+    
+    # Extract info from dataloader and run network
+    for epoch in range(args.epochs):
+        count = 0
+        for snp, pos, label in dataloader:
+            count += 1
 
-tensor = torch.from_numpy(sim)
-tensor = tensor.double()
-tensor = tensor.unsqueeze(dim = 1)
+            # Reshape each chunk of training examples
+            snp = snp.view(-1, ds.num_indivs,
+                           ds.num_sites).unsqueeze(1)
+            pos = pos.view(-1, ds.num_sites)
+            print(f"label shape before reshape: {label.shape}")
+            label = label.view(-1)
+            label_ohe = F.one_hot(label)
+            label_ohe = label_ohe.to(torch.float32)
 
-new_p = np.array([list(item) for item in param])
-new_p = new_p[:, 2:7]
-new_p = torch.from_numpy(np.array(new_p))
-# print(new_p.shape)
+            # Perform one forward pass
+            out = net(snp, pos)
+            out = out.to(torch.float32)
+            print(f"net output shape: {out.shape}")
+            print(f"label tensor shape: {label.shape}")
+            loss = F.mse_loss(out, label_ohe).cuda()
+            
+            # Perform backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (count) % 100 == 0 and gpu == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                    epoch + 1,
+                    args.epochs,
+                    count,
+                    total_step,
+                    loss.item())
+                   )
+                
+    toc = time.perf_counter()
+    print(f"Total time run: {(toc-tic)/60:0.4f} minutes")
 
-train = torch.utils.data.TensorDataset(tensor, new_p)
-
-trainloader = torch.utils.data.DataLoader(train, batch_size = 20,
-                                          shuffle = True)
-# print(trainloader)
-# tensor = tensor.unsqueeze(dim = 1)
-# print(tensor.shape)
-# print(tensor.shape)
-#t1 = tensor[1,1]
-
-# Build the net
-
-class Net(nn.Module):
-    # Define all the transformations we want here
-    def __init__(self):
-        # Inherit behaviour from PyTorch class
-        super(Net, self).__init__()
-        # Convolutional transforms
-        # input to Conv2d should be a 4D tensor [batch_size, channel, l, w]
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(4, 4)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # Regular neural net transforms after unwrap
-        self.fc1 = nn.Linear(16 * 3 * 77, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 5)
-
-    # Define all the steps for one full forward pass
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        # print(x.shape)
-        x = x.view(-1, 16 * 3 * 77)
-        # print(x.shape)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-net = Net()
-
-# Loss and optimizer functions
-
-criterion = F.mse_loss
-optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.7)
-
-n_epoch = 20
-
-net = net.double()
-
-# Load CUDA if present
-# to load CUDA in colab, go to runtime->change runtime type and select gpu
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("Using device: %s" % device)
-
-net.to(device)
-
-for epoch in range(n_epoch): # loope over dataset multiple times
-    running_loss = 0.0
-    count_b = 0
-    for inputs, labels in trainloader:
-        count_b += 1
-        # data is a list of [inputs, labels]
-        #inputs, labels = data[0], data[1] # note that moving tensors to the device is done _out of place_
-        inputs, labels = inputs.to(device), labels.to(device)
-        # print(f"labels as long: {labels.long()}")
-        # print(inputs)
-        #inputs = tensor[i,:]
-        # zero parameter gradients
-        optimizer.zero_grad()
-
-        # forward -> backward -> optimize
-        outputs = net(inputs)
-        # print(f"output shape: {outputs.shape}" )
-        # print(f"label shape: {labels.shape}" )
-        # outputs = outputs.unsqueeze(dim=0)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if count_b % 5 == 4: # print every 20 mini-batches
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, count_b + 1, running_loss / 5))
-            running_loss = 0.0
-
-print('Finished Training')
-
-toc = time.perf_counter()
-print(f"Total time run: {(toc-tic)/60:0.4f} minutes")
-
-### Where we save to needs to be modified!! ###
-torch.save(net.state_dict(), "./test.pth")
+if __name__ == '__main__:
+    if __name__ == '__main__':
+    import cProfile, pstats
+    profiler = cProfile.Profile()
+    profiler.enable()
+    main()
+    profiler.disable()
+    stats = pstats.Stats(profiler)
+    stats.strip_dirs()
+    stats.sort_stats('tottime')
+    stats.print_stats()
+    stats.dump_stats('./profiler/CNN_draft.prof')
